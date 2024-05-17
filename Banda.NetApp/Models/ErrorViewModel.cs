f3974e68122b85373d1139be9b6f8ce769dd5ccf
jonatas1096 <jonatas1096@hotmail.com>
2024-05-16 23:45:10 -0300

    armazenando para estudos

---
+++ Banda.NetApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Banda.NetApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
