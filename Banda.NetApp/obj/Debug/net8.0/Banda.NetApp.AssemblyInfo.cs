469f25c8432c244a056c62680a9339e0341a48b1
jonatas1096 <jonatas1096@hotmail.com>
2024-06-10 00:37:00 -0300

    swiper slide (prototype) with members description

--- Banda.NetApp/obj/Debug/net8.0/Banda.NetApp.AssemblyInfo.cs
+++ Banda.NetApp/obj/Debug/net8.0/Banda.NetApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Banda.NetApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5979f2e03c07fa413c857f4c736320fc3df08e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+261d9a7e4718180f64d4e5d489c8fd792affe237")]
 [assembly: System.Reflection.AssemblyProductAttribute("Banda.NetApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Banda.NetApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Banda.NetApp/wwwroot/js/site.js
+++ Banda.NetApp/wwwroot/js/site.js
@@ -2,8 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-document.addEventListener("DOMContentLoaded", function () {
 
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    //
 
     function darkNavbar() {
 
@@ -26,8 +31,75 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     darkNavbar();
 
+    //
+
+
+    //Initializing swiper carousel (library)
+    const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+        effect: 'coverflow',
+        centeredSlides: true,
+        grabCursor: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 0.9,
+            slideShadows: true,
+        },
+        // pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        on: {
+            slideChange: function (){
+                const activeIndex = this.activeIndex; //getting the actual index
+
+                const activeSlide = this.slides[activeIndex]; //select the slide of this index
+                const activeImg = activeSlide.querySelector('img'); //In this slide, getting the image
+
+                const memberRole = document.querySelector('.aboutMember h2');
+                const memberDescription = document.querySelector('.aboutMember p')
+                const memberLink = document.querySelector('.aboutMember a')
+                
+                if (activeImg.id === 'felipe') {
+                    memberRole.textContent = 'VOCALISTA';
+                    memberDescription.textContent = '“Felipe Takashi”: vocalista, fundador da ópio, homossexual publicamente assumido, grunge e fodido financeiramente, é o antecessor de bla, lorem ipsum lorem, bla bla bla bla bla, lorem ipsum lorembla bla bla bla bla, lorem ipsum lorem"'
+                    memberLink.href = 'https://www.instagram.com/felipe.takashii/'
+                }
+                else if (activeImg.id === 'enzo') {
+                    memberRole.textContent = 'BATEIRISTA';
+                    memberDescription.textContent = '“Batuca mucho"'
+                    memberLink.href = 'https://www.instagram.com/enzoispunk/'
+                }
+                else if (activeImg.id === 'ygor') {
+                    memberRole.textContent = 'GUITARRISTA';
+                    memberDescription.textContent = '“Toca mucha guitarita"'
+                    memberLink.href = 'https://www.instagram.com/ygorleaoo_/'
+                }
+                else if (activeImg.id === 'gustavo') {
+                    memberRole.textContent = 'GUITARRISTA';
+                    memberDescription.textContent = '“Toca mucha guitarita"'
+                    memberLink.href = 'https://www.instagram.com/gushikenmusic/'
+                }
+                else {
+                    memberRole.textContent = 'GATO';
+                    memberDescription.textContent = '“Mascote oficial da banda ópio."'
+                    memberLink.href = 'https://www.petz.com.br/blog/pets/gatos/perfil-de-gato/'
+                }
+
+            }
+        }
+    });
+
+    
 
 });
+
+
 
 
     function blurMenuDrop() { //turn on or off the blur filter.
